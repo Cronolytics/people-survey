f5989c3d2edfd7cf5528f87ac5140b3244008aed
Gustavo C O Martins <gustavo.omartins@bandtec.com.br>
2022-11-15 18:48:45 -0300

    clean

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import '../assets/css/reset.css'
 import '../assets/css/dash-style.css'
 import CardPesquisa from "../components/dashboard-components/card-pesquisa-component/CardPesquisa";
-import { Pie } from 'react-chartjs-2';
+//import { Pie } from 'react-chartjs-2';
 
 function Dashboard(){
 
@@ -170,30 +170,7 @@ function Dashboard(){
 
                                 <div className="chart-box">
                                     <div>
-                                    <Pie    data={{
-                                                labels: ['Red', 'Blue', 'Yellow'],
-                                                datasets: [{ 
-                                                    label: '# of votes',                             
-                                                    data: [300, 50, 100],
-                                                    backgroundColor: [
-                                                        'rgb(255, 99, 132)',
-                                                        'rgb(54, 162, 235)',
-                                                        'rgb(255, 205, 86)'
-                                                    ],
-                                                    borderColor: [
-                                                        'rgb(255, 99, 132)',
-                                                        'rgb(54, 162, 235)',
-                                                        'rgb(255, 205, 86)'
-                                                    ],
-                                                    hoverOffset: 4
-                                                }]
-                                            }}
-                                            height={300}
-                                            width={300}
-                                            options={{
-                                                maintainAspectRatio: false,
-                                            }}
-                                        />
+                                    
                                     </div>
                                 </div>                                
                             </div>                         
