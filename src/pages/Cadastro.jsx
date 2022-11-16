c92dc4548d76c89a2025cc48dc4bb837802ec268
anthoybuenoo <anthoy.bueno@bandtec.com.br>
2022-11-16 19:39:10 -0300

    atualizacoes menu, login e cadastro

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -11,6 +11,51 @@ const Cadastro = () => {
 
     const navigate = useNavigate();
 
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const [telefone, setTelefone] = useState("");
+    const [cep, setCep] = useState("");
+
+    async function cadastrarEmpresa() {
+        try {
+            const user = await api.post('/empresa', {
+                nome,
+                email,
+                senha,
+                telefone,
+                cep
+            });
+
+            sessionStorage.clear();
+            sessionStorage.setItem("survey-manager", JSON.stringify(user.data));
+
+            Toastify({
+                text: "Cadastro bem sucedido!",
+                duration: 3000,
+                close: true,
+                gravity: "top", // `top` or `bottom`
+                position: "right", // `left`, `center` or `right`
+                stopOnFocus: true, // Prevents dismissing of toast on hover
+                style: { background: "linear-gradient(to right, #00b09b, #96c93d)" }
+            }).showToast();
+
+            navigate('/login');
+        } catch (error) {
+
+            Toastify({
+                text: "Ops! Algo deu errado...",
+                duration: 3000,
+                close: true,
+                gravity: "top", // `top` or `bottom`
+                position: "right", // `left`, `center` or `right`
+                stopOnFocus: true, // Prevents dismissing of toast on hover
+                style: { background: "linear-gradient(to right, #a8323c, #e00d1f)" }
+            }).showToast();
+        }
+    }
+
+
     return (
         <>
             <div className="img-logo">
@@ -23,35 +68,23 @@ const Cadastro = () => {
                         <p>Crie sua conta</p>
                     </div>
 
-                    <label>E-mail</label>
-                    <div className="ui left icon input inputCurriculo"><i aria-hidden="true" /><input onChange={(e) => setEmail(e.target.value)} type="text" placeholder="Email" /></div>
-
-                    <label>Senha</label>
-                    <div className="ui left icon input inputCurriculo"><i aria-hidden="true" /><input onChange={(e) => setSenha(e.target.value)} type="password" placeholder="Senha" /></div>
+                    <label>Nome </label>
+                    <div className="ui left icon input inputCurriculo"><i aria-hidden="true" /><input onChange={(e) => setNome(e.target.value)} type="text" placeholder="Nome" /></div>
 
                     <label>E-mail</label>
-                    <div className="ui left icon input inputCurriculo"><i aria-hidden="true" /><input onChange={(e) => setEmail(e.target.value)} type="text" placeholder="Email" /></div>
+                    <div className="ui left icon input inputCurriculo"><i aria-hidden="true" /><input onChange={(e) => setEmail(e.target.value)} type="password" placeholder="E-mail" /></div>
 
                     <label>Senha</label>
-                    <div className="ui left icon input inputCurriculo"><i aria-hidden="true" /><input onChange={(e) => setSenha(e.target.value)} type="password" placeholder="Senha" /></div>
+                    <div className="ui left icon input inputCurriculo"><i aria-hidden="true" /><input onChange={(e) => setSenha(e.target.value)} type="text" placeholder="Senha" /></div>
+
+                    <label>Telefone</label>
+                    <div className="ui left icon input inputCurriculo"><i aria-hidden="true" /><input onChange={(e) => setTelefone(e.target.value)} type="password" placeholder="Telefone" /></div>
 
                     <label>Senha</label>
-                    <div className="ui left icon input inputCurriculo"><i aria-hidden="true" /><input onChange={(e) => setSenha(e.target.value)} type="password" placeholder="Senha" /></div>
-
-                    <label>Senha</label>
-                    <div className="ui left icon input inputCurriculo"><i aria-hidden="true" /><input onChange={(e) => setSenha(e.target.value)} type="password" placeholder="Senha" /></div>
-
-                    <label>Senha</label>
-                    <div className="ui left icon input inputCurriculo"><i aria-hidden="true" /><input onChange={(e) => setSenha(e.target.value)} type="password" placeholder="Senha" /></div>
-
-                    <label>Senha</label>
-                    <div className="ui left icon input inputCurriculo"><i aria-hidden="true" /><input onChange={(e) => setSenha(e.target.value)} type="password" placeholder="Senha" /></div>
-
-                    <label>Senha</label>
-                    <div className="ui left icon input inputCurriculo"><i aria-hidden="true" /><input onChange={(e) => setSenha(e.target.value)} type="password" placeholder="Senha" /></div>
+                    <div className="ui left icon input inputCurriculo"><i aria-hidden="true" /><input onChange={(e) => setCep(e.target.value)} type="password" placeholder="Cep" /></div>
 
                     <div>
-                        <button onClick={() => verifyCredentials()} className="botao-login">Entrar</button>
+                        <button onClick={() => cadastrarEmpresa()} className="botao-login">Entrar</button>
                     </div>
 
                 </form>
