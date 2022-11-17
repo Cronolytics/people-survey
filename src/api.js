690644262b11ab9cc9ac3461072091d58f97f9a6
Guilherme Henrique dos Santos <89214111+Gui-henrique-dos-santos@users.noreply.github.com>
2022-11-16 21:33:07 -0300

    novas atualizações
    atualizações

--- src/api.js
+++ src/api.js
@@ -1,14 +1,5 @@
 import axios from "axios";
 
-// const apiDev = axios.create({
-//     baseURL: "https://localhost:8080"
-// })
-
-// const apiProd = axios.create({
-//     baseURL: "https://peoplesurvey.azurewebsites.net"
-// })
-
-
 const baseURL = (process.env.NODE_ENV !== 'production') ? 'http://localhost:8080' : 'https://peoplesurvey.azurewebsites.net'; 
 
 const api = axios.create({ baseURL });
--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,48 +1,51 @@
-import '../assets/css/reset.css'
-import '../assets/css/login-cadastro.css'
+import "../assets/css/reset.css";
+import "../assets/css/login-cadastro.css";
 
-import logo_survey from '../assets/images/logo-survey.png'
-import CampoTexto from '../components/component-login-cadastro/input-cadastro-component/input-register';
-import BotaoCadastro from '../components/component-login-cadastro/cadastro-button-component/button-register'
-import { useNavigate } from 'react-router-dom';
+import logo_survey from "../assets/images/logo-survey.png";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import api from "../api";
+import Toastify from "toastify-js";
+import "toastify-js/src/toastify";
 
-
-const Cadastro = () => {
-
+function Cadastro(){
     const navigate = useNavigate();
-
     const [nome, setNome] = useState("");
     const [email, setEmail] = useState("");
     const [senha, setSenha] = useState("");
+    const [confirmarsenha, setConfirmarSenha] = useState("");
     const [telefone, setTelefone] = useState("");
     const [cep, setCep] = useState("");
+    const [cnpj, setCnpj] = useState("");
 
     async function cadastrarEmpresa() {
         try {
-            const user = await api.post('/empresa', {
-                nome,
-                email,
-                senha,
-                telefone,
-                cep
-            });
+            const user = await api.post("/empresa", {
+            nome,
+            email,
+            senha,
+            telefone,
+            cep,
+            cnpj,
+        });
 
-            sessionStorage.clear();
-            sessionStorage.setItem("survey-manager", JSON.stringify(user.data));
+        console.log(user.data);
+        sessionStorage.clear();
+        sessionStorage.setItem("survey-manager", JSON.stringify(user.data));
 
-            Toastify({
-                text: "Cadastro bem sucedido!",
-                duration: 3000,
-                close: true,
-                gravity: "top", // `top` or `bottom`
-                position: "right", // `left`, `center` or `right`
-                stopOnFocus: true, // Prevents dismissing of toast on hover
-                style: { background: "linear-gradient(to right, #00b09b, #96c93d)" }
-            }).showToast();
+        Toastify({
+            text: "Cadastro bem sucedido!",
+            duration: 3000,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: { background: "linear-gradient(to right, #00b09b, #96c93d)" },
+        }).showToast();
 
-            navigate('/login');
+        navigate("/login");
         } catch (error) {
-
+            console.log(error);
             Toastify({
                 text: "Ops! Algo deu errado...",
                 duration: 3000,
@@ -50,53 +53,118 @@ const Cadastro = () => {
                 gravity: "top", // `top` or `bottom`
                 position: "right", // `left`, `center` or `right`
                 stopOnFocus: true, // Prevents dismissing of toast on hover
-                style: { background: "linear-gradient(to right, #a8323c, #e00d1f)" }
+                style: { background: "linear-gradient(to right, #a8323c, #e00d1f)" },
             }).showToast();
         }
     }
 
-
+    
     return (
         <>
             <div className="img-logo">
-                <img onClick={() => navigate("/")} src={logo_survey} alt='' />
+                <img onClick={() => navigate("/")} src={logo_survey} alt="" />
             </div>
 
-            <div className='container-cadastro'>
-                <form >
+            <div className="container-cadastro">
+                <form>
                     <div className="div-texto-cadastro">
                         <p>Crie sua conta</p>
                     </div>
 
-                    <label>Nome </label>
-                    <div className="ui left icon input inputCurriculo"><i aria-hidden="true" /><input onChange={(e) => setNome(e.target.value)} type="text" placeholder="Nome" /></div>
+                    <div className="input-container">
+                        <div>
+                            <label>Nome</label>
+                            <div className="ui left icon input inputCurriculo">
+                                <i aria-hidden="true" />
+                                <input
+                                onChange={(e) => setNome(e.target.value)}
+                                type="text"
+                                placeholder="Nome"
+                                />
+                            </div>
+                        </div>
 
-                    <label>E-mail</label>
-                    <div className="ui left icon input inputCurriculo"><i aria-hidden="true" /><input onChange={(e) => setEmail(e.target.value)} type="password" placeholder="E-mail" /></div>
+                        <div>
+                            <label>E-mail</label>
+                            <div className="ui left icon input inputCurriculo">
+                                <i aria-hidden="true" />
+                                <input
+                                onChange={(e) => setEmail(e.target.value)}
+                                type="email"
+                                placeholder="E-mail"
+                                />
+                            </div>
+                        </div>
 
-                    <label>Senha</label>
-                    <div className="ui left icon input inputCurriculo"><i aria-hidden="true" /><input onChange={(e) => setSenha(e.target.value)} type="text" placeholder="Senha" /></div>
+                        <div>
+                            <label>Senha</label>
+                            <div className="ui left icon input inputCurriculo">                           
+                                <i aria-hidden="true"/>
+                                <input type="password"
+                                onChange={(e) => setSenha(e.target.value)}
+                                placeholder="Senha"
+                                />                          
+                            </div>
+                        </div>
 
-                    <label>Telefone</label>
-                    <div className="ui left icon input inputCurriculo"><i aria-hidden="true" /><input onChange={(e) => setTelefone(e.target.value)} type="password" placeholder="Telefone" /></div>
+                        <div>
+                            <label>Confirmar senha</label>
+                            <div className="ui left icon input inputCurriculo">
+                                <i aria-hidden="true"/>
+                                <input 
+                                onChange={(e) => setConfirmarSenha(e.target.value)}                               
+                                placeholder="Confirme sua senha"
+                                type="password"                           
+                                />
+                            </div>                        
+                        </div>
 
-                    <label>Senha</label>
-                    <div className="ui left icon input inputCurriculo"><i aria-hidden="true" /><input onChange={(e) => setCep(e.target.value)} type="password" placeholder="Cep" /></div>
+                        <div>
+                            <label>Telefone</label> 
+                            <div className="ui left icon input inputCurriculo">                                                        
+                                <i aria-hidden="true" />
+                                <input type="text"
+                                onChange={(e) => setTelefone(e.target.value)}
+                                placeholder="Telefone"
+                                />
+                            </div>       
+                        </div>
 
-                    <div>
-                        <button onClick={() => cadastrarEmpresa()} className="botao-login">Entrar</button>
+                        <div>
+                            <label>Cep</label>                     
+                            <div className="ui left icon input inputCurriculo">
+                                <i aria-hidden="true" />                       
+                                <input
+                                onChange={(e) => setCep(e.target.value)}
+                                placeholder="Cep"
+                                type="text"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <label>CNPJ</label> 
+                            <div className="ui left icon input inputCurriculo">                                                             
+                                <i aria-hidden="true"/>
+                                <input type="text"
+                                onChange={(e) => setCnpj(e.target.value)}
+                                placeholder="Cnpj"
+                                />                      
+                            </div>
+                        </div>
+
+                        <div>
+                            <button
+                                onClick={() => cadastrarEmpresa()}
+                                className="botao-cadastro">
+                                Cadastrar
+                            </button>
+                        </div>
                     </div>
-
                 </form>
             </div>
-
-
-
-
-
-
         </>
-    );
-}
+    )
+};
 
 export default Cadastro;
