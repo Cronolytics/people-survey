418587ed6472a25e3b89d73102753f0401dc9ed2
anthoybuenoo <anthoy.bueno@bandtec.com.br>
2022-11-18 16:08:31 -0300

    atualização de navbar

--- src/pages/MinhasPesquisas.jsx
+++ src/pages/MinhasPesquisas.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import '../assets/css/reset.css'
-import '../assets/css/style.css'
+import '../assets/css/minhas-pesquisas.css'
 import Menu from '../components/menu-conponents/NavbarMenu'
 
 function MinhasPesquisas() {
+
   return (
+  
     <div>
       <div className="navbar-menu">
         <Menu />
-        {/* <input type="text" /> */}
+        <div className="titlle-nav">
+            <h1>Minhas pesquisas</h1>
+        </div>
       </div>
 
     </div>
