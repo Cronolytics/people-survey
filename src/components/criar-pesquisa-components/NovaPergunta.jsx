c5a91066559d99966208a60b5305f3bdb7fc95c4
Gustavo C O Martins <gustavo.omartins@bandtec.com.br>
2022-11-19 12:06:31 -0300

    atualizacoes nova pesquisa

--- src/components/criar-pesquisa-components/NovaPergunta.jsx
+++ src/components/criar-pesquisa-components/NovaPergunta.jsx
@@ -1,26 +1,40 @@
 import React from 'react'
-import { Button, Card, Form } from 'semantic-ui-react'
+import { Form } from 'semantic-ui-react'
+import './nova-pergunta-style.css'
 
 function NovaPergunta(){
+
+    function novaResposta(){
+        alert("teste")
+    }
+
     return(
         <>
-        <Card.Group>
-            <Card>
-                <Card.Content>
-                    <Form.Field label='Digite a pergunta' control='input' />
-                </Card.Content>
-                <Card.Content extra>
-                    <div className='ui two buttons'>
-                        <Button basic color='green'>
-                            Approve
-                        </Button>
-                        <Button basic color='red'>
-                            Decline
-                        </Button>
-                    </div>
-                </Card.Content>
-            </Card>
-        </Card.Group>    
+        <div className="card-pergunta">
+            <div className="card-box">         
+                <Form.Field label='Pergunta' control='textarea' />
+            </div>
+            <div className="card-box">            
+                <form className="ui form">
+                    <div className="field"><b>Respostas:</b></div>
+                    <div className="field w-100">                    
+                        <div className='resposta-box'>
+                            <div className='contador-box'>
+                                1
+                            </div>
+                            <div className="ui fluid icon input">
+                                <div className="ui transparent input">
+                                    <input onDragEnter={novaResposta} type="text" placeholder="Digite uma opção de resposta."/>
+                                </div>
+                                <button className="ui icon button button-limiter">
+                                    <i aria-hidden="true" className="delete icon button-limiter"></i>
+                                </button>
+                            </div>                           
+                        </div>
+                    </div>                  
+                </form>
+            </div>
+        </div>
         </>
     )
 }
