d26f2890dd21ecd6004ca5f0e8c32762b4d91da1
Gui-henrique-dos-santos <guilherme.hsantos@bandtec.com.br>
2023-06-12 09:00:28 -0300

    feat: portas atualizadas

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 var ambiente = "production";
-const baseURL = (ambiente !== 'production') ? 'http://localhost:8080' : 'http://3.92.171.109:8080'; 
+const baseURL = (ambiente !== 'production') ? 'http://localhost:8080' : 'http://3.92.171.109:8081'; 
 const api = axios.create({ baseURL });
 
 export default api;
